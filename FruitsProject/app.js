4abc2b616f417b4a943f5bd44d23523681d6afcd
Navalona501 <119346839+Navalona501@users.noreply.github.com>
2023-03-23 17:12:12 +0100

    mongoose not working

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -5,16 +5,31 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb://localhost:27017/fruitsDB', {useNewUrlParser: true, useUnifiedTopology: true});
-
-const fruitSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
-    review: String
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  console.log('Connected to database');
+}).catch((err) => {
+  console.log('Error connecting to database: ', err);
 });
 
-app.set('view engine', 'ejs');
+
+// const fruitSchema = new mongoose.Schema({
+//     name: String,
+//     rating: Number,
+//     review: String
+// });
+
+// const Fruit = mongoose.model('Fruit', fruitSchema);
+
+// const fruit1 = new Fruit({
+//     name: "Apple",
+//     rating: 7,
+//     review: "Great fruit"
+// });
+
+// fruit1.save();
+
 
 app.listen(3000, () => {
   console.log('Server started on port 3000 ');
