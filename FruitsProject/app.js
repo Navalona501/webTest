6c925943605c5540f95b535c0cd9754161d48574
Navalona501 <119346839+Navalona501@users.noreply.github.com>
2023-03-23 18:56:22 +0100

    mongoose test work

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -2,33 +2,36 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/fruitsDB', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
   console.log('Connected to database');
 }).catch((err) => {
   console.log('Error connecting to database: ', err);
 });
 
 
-// const fruitSchema = new mongoose.Schema({
-//     name: String,
-//     rating: Number,
-//     review: String
-// });
+const fruitSchema = new mongoose.Schema({
+    name: String,
+    rating: Number,
+    review: String
+});
 
-// const Fruit = mongoose.model('Fruit', fruitSchema);
+const Fruit = mongoose.model('Fruit', fruitSchema);
 
-// const fruit1 = new Fruit({
-//     name: "Apple",
-//     rating: 7,
-//     review: "Great fruit"
-// });
+const fruit1 = new Fruit({
+    name: "Apple",
+    rating: 7,
+    review: "Great fruit"
+});
 
-// fruit1.save();
+fruit1.save();
 
 
 app.listen(3000, () => {
